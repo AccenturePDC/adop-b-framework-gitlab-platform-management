57bbbd41ba74faa5396be8aa05591e7dbe606806
John Bryan Sazon <johnbryansazon@Johns-MacBook-Air.local>
2016-09-18 02:38:29 +0800

    restructured the script

--- bootstrap/Platform_Management/Load_Openshift_Platform.groovy
+++ bootstrap/Platform_Management/Load_Openshift_Platform.groovy
@@ -31,7 +31,7 @@ loadPlatformExtensionJob.with{
         stringParam("AWS_VPC_ID","$vpcId","")
         stringParam("AWS_VPC_CIDR","$vpcCidr","")
         stringParam("MASTERS_INSTANCE_TYPE",'t2.medium',"")
-        stringParam("NODES_INSTANCE_TYPE",'t2.medium',"")
+        stringParam("MINIONS_INSTANCE_TYPE",'t2.medium',"")
         stringParam("REMOTE_SUDO_USER",'centos',"")
         stringParam("OPENSHIFT_ADMIN_USER",'admin',"")
         nonStoredPasswordParam("OPENSHIFT_ADMIN_PASSWORD","")
@@ -47,7 +47,7 @@ loadPlatformExtensionJob.with{
     scm{
         git{
             remote{
-                url('https://github.com/AccenturePDC/ansible-ec2-create')
+                url('https://github.com/AccenturePDC/ansible-ec2-create.git')
             }
             branch('*/master')
         }
@@ -68,32 +68,16 @@ loadPlatformExtensionJob.with{
 
 export ANSIBLE_FORCE_COLOR=true
 
-# -----------------------------------------------------------------
-# Create EC2 openshift nodes
-# -----------------------------------------------------------------
+echo "# -----------------------------------------------------------------"
+echo "# Create Openshift Master EC2"
+echo "# -----------------------------------------------------------------"
 
-rm -f instances_data.sh
+export MASTER_INSTANCE_NAME=Openshift_Master_Node
+ROOT_VOLUME_SIZE=20
+DOCKER_VOLUME_SIZE=20
+SUBNET=${AWS_PUBLIC_SUBNET_ID}
 
-## Create Openshift master
-export INSTANCE_NAME=Openshift_Master_Node
-VOLUME_SIZE=20
-
-ansible-playbook provision.yml --extra-vars "instance_name=${INSTANCE_NAME} aws_region=${AWS_REGION} key_name=${AWS_KEY_PAIR} vpc_subnet_id=${AWS_PUBLIC_SUBNET_ID} ami_id=${CENTOS_AMI_ID} instance_type=${MASTERS_INSTANCE_TYPE} volume_size=${VOLUME_SIZE} vpc_id=${AWS_VPC_ID} volume_device_name='/dev/sda1' env=openshift_cluster vpc_cidr_block=${AWS_VPC_CIDR} type=platform"
-
-# Create temporary playbook to attach volume
-cat > attach_vol.yml <<-EOF
----
-- hosts: '127.0.0.1\'
-  connection: local
-  become: true
-  become_user: root
-  gather_facts: False
-  roles:
-   - attach_volume
-EOF
-
-VOLUME_SIZE=30
-ansible-playbook attach_vol.yml -i hosts -u ${REMOTE_SUDO_USER} --extra-vars "aws_region=${AWS_REGION} lv_volume_size=${VOLUME_SIZE}" -t "attach_volume"
+ansible-playbook provision.yml --extra-vars "instance_name=${MASTER_INSTANCE_NAME} aws_region=${AWS_REGION} key_name=${AWS_KEY_PAIR} vpc_subnet_id=${SUBNET} ami_id=${CENTOS_AMI_ID} instance_type=${MASTERS_INSTANCE_TYPE} volume_size=${ROOT_VOLUME_SIZE} lv_volume_size=${DOCKER_VG_VOLUME_SIZE} vpc_id=${AWS_VPC_ID} volume_device_name='/dev/sda1' env=openshift_cluster vpc_cidr_block=${AWS_VPC_CIDR}"
 
 if [ $? -gt 0 ]
 then
@@ -101,26 +85,17 @@ then
   exit 1
 fi
 
-## Create Openshift Node
-export INSTANCE_NAME=Openshift_Minion_Node
-VOLUME_SIZE=20
 
-ansible-playbook provision.yml --extra-vars "instance_name=${INSTANCE_NAME} aws_region=${AWS_REGION} key_name=${AWS_KEY_PAIR} vpc_subnet_id=${AWS_PRIVATE_SUBNET_ID} ami_id=${CENTOS_AMI_ID} instance_type=${MASTERS_INSTANCE_TYPE} volume_size=${VOLUME_SIZE} vpc_id=${AWS_VPC_ID} volume_device_name='/dev/sda1' env=openshift_cluster vpc_cidr_block=${AWS_VPC_CIDR} type=platform assign_public_ip=no"
+echo "# -----------------------------------------------------------------"
+echo "# Create Openshift Minion EC2"
+echo "# -----------------------------------------------------------------"
 
-# Create temporary playbook to attach volume
-cat > attach_vol.yml <<-EOF
----
-- hosts: '127.0.0.1\'
-  connection: local
-  become: true
-  become_user: root
-  gather_facts: False
-  roles:
-   - attach_volume
-EOF
+export MINION_INSTANCE_NAME=Openshift_Minion_Node
+ROOT_VOLUME_SIZE=20
+DOCKER_VOLUME_SIZE=40
+SUBNET=${AWS_PRIVATE_SUBNET_ID}
 
-VOLUME_SIZE=30
-ansible-playbook attach_vol.yml -i hosts -u ${REMOTE_SUDO_USER} --extra-vars "aws_region=${AWS_REGION} lv_volume_size=${VOLUME_SIZE}" -t "attach_volume"
+ansible-playbook provision.yml --extra-vars "instance_name=${MINION_INSTANCE_NAME} aws_region=${AWS_REGION} key_name=${AWS_KEY_PAIR} vpc_subnet_id=${SUBNET} ami_id=${CENTOS_AMI_ID} instance_type=${MINIONS_INSTANCE_TYPE} volume_size=${ROOT_VOLUME_SIZE} lv_volume_size=${DOCKER_VG_VOLUME_SIZE} vpc_id=${AWS_VPC_ID} volume_device_name='/dev/sda1' env=openshift_cluster vpc_cidr_block=${AWS_VPC_CIDR}"
 
 if [ $? -gt 0 ]
 then
@@ -128,78 +103,58 @@ then
   exit 1
 fi
 
-# -----------------------------------------------------------------
-# Create the ansible host file for Openshift Ansible installation
-# -----------------------------------------------------------------
+echo "# -----------------------------------------------------------------"
+echo "# Create the ansible host file for Openshift Ansible installation"
+echo "# -----------------------------------------------------------------"
 
-export $(cat instances_data.sh)
-
-rm -fr openshift && mkdir openshift && cd openshift
+export $(cat instances_data.txt)
 
 cat > openshift-hosts <<EOF
 # Create an OSEv3 group that contains the masters and nodes groups
 [OSEv3:children]
 masters
 nodes
-
 # Set variables common for all OSEv3 hosts
 [OSEv3:vars]
 # SSH user, this user should allow ssh based auth without requiring a password
 ansible_ssh_user=centos
-
 # If ansible_ssh_user is not root, ansible_sudo must be set to true
 ansible_sudo=true
-
 deployment_type=origin
-
 # uncomment the following to enable htpasswd authentication; defaults to DenyAllPasswordIdentityProvider
 openshift_master_identity_providers=[{'name': 'htpasswd_auth', 'login': 'true', 'challenge': 'true', 'kind': 'HTPasswdPasswordIdentityProvider', 'filename': '/etc/origin/master/htpasswd'}]
-
 # Use something like apps.<public-ip>.xip.io if you don't have a custom domain
 openshift_master_default_subdomain=apps.${Openshift_Master_Node_PUBLIC_IP}.xip.io
-
 [masters]
 ${Openshift_Master_Node_PUBLIC_HOSTNAME}
-
 [nodes]
 # Make the master node schedulable by default
 ${Openshift_Master_Node_PUBLIC_HOSTNAME} openshift_node_labels="{'region': 'infra', 'zone': 'default'}" openshift_schedulable=true
-
 # You can add some nodes below if you want!
 ${Openshift_Minion_Node_PRIVATE_HOSTNAME} openshift_node_labels="{'region': 'primary', 'zone': 'default'}"
 EOF
 
-# -----------------------------------------------------------------
-# Install Openshift Prerequisite on Cluster
-# -----------------------------------------------------------------
+echo "# -----------------------------------------------------------------"
+echo "# Install Openshift Prerequisite on Cluster"
+echo "# -----------------------------------------------------------------"
 
-git clone https://github.com/bzon/openshift-ansible-draft.git
+git clone https://github.com/AccenturePDC/ansible-openshift-roles.git
+cd ansible-openshift-roles
+ansible-playbook -i ../openshift-hosts install_req.yml -e "device_name=${DEVICE_NAME}"
 
-cd openshift-ansible-draft
-
-cat > install_req.yml <<EOF
-- hosts: "OSEv3:children"
-  roles:
-    - openshift_prereq
-EOF
-
-ansible-playbook -i ../openshift-hosts install_req.yml -e "device_name=/dev/xvdf"
-
-# -----------------------------------------------------------------
-# Install Openshift Cluster
-# -----------------------------------------------------------------
+echo "# -----------------------------------------------------------------"
+echo "# Install Openshift Cluster"
+echo "# -----------------------------------------------------------------"
 
 cd ../
 git clone https://github.com/openshift/openshift-ansible -b master
 cd openshift-ansible
-
 yum -y install pyOpenSSL
-
 ansible-playbook playbooks/byo/config.yml -u ${REMOTE_SUDO_USER} --become --become-user root -i ../openshift-hosts
 
-# -----------------------------------------------------------------
-# Create initial cluster-admin user
-# -----------------------------------------------------------------
+echo "# -----------------------------------------------------------------"
+echo "# Create initial cluster-admin user"
+echo "# -----------------------------------------------------------------"
 
 ansible masters -i ../openshift-hosts -m shell -a "htpasswd -b /etc/origin/master/htpasswd ${OPENSHIFT_ADMIN_USER} ${OPENSHIFT_ADMIN_PASSWORD}"
 ansible masters -i ../openshift-hosts -m shell -a "oadm policy add-role-to-user cluster-admin ${OPENSHIFT_ADMIN_USER}"
